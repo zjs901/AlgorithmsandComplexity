5e4785af9122a856ccf4c4ebbc9c5d863d4382fa
zjs901 <zakjs901@gmail.com>
2025-03-15 14:42:37 +0000

    Added a little menu

--- Algorithms Assignment/Program.cs
+++ Algorithms Assignment/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Application 
+{
+    
+    class main 
+    {
+        static void Main(string[] args) 
+        {
+            while (true)
+            {
+                Console.Write("Menu:");
+                Console.WriteLine("1. Sort");
+                Console.WriteLine("2. Search");
+                string menuInput = Console.ReadLine();
+
+                if (menuInput == "1")
+                {
+                    Console.WriteLine("Sorting...");
+                }
+            }
+        }
+    }
+}
