d3741fcf4213a03d313ff9deb6a003cf9b64e844
zjs901 <zakjs901>
2025-03-19 20:04:34 +0000

    Completed the insertion sort algorithm as well as the search function and reading all the files

--- Algorithms Assignment/Program.cs
+++ Algorithms Assignment/Program.cs
@@ -10,95 +10,194 @@ namespace Application
 {
     class MainClass
     {
+        public static int[] numbersArray1;
+        public static int[] numbersArray2;
+        public static int[] numbersArray3;
+        public static int[] numbersArray4;
+        public static int[] numbersArray5;
+        public static int[] numbersArray6;
+
+
         static void Main(string[] args) 
         {
+            ReadFiles();
 
-            List<int> dataSet = new List<int>();
+            //int[] testArray = { 2, 5, 7, 1, 2 };
 
-            // Reads the file and converts it into an array
-            string filePath = "C:\\Users\\UpInT\\Source\\Repos\\AlgorithmsandComplexity1\\Algorithms Assignment\\Share_1_256.txt";
-            List<string> lines = File.ReadAllLines(filePath).ToList();
-            int[] myNum = File.ReadAllLines(filePath).Select(int.Parse).ToArray();
+            //InsertionSort(numbersArray4);
+            InsertionSort(numbersArray1);
+            PrintArray(numbersArray1);
+            Search(numbersArray1);
 
-            // Adds data to the data set list
-            foreach (string line in lines)
+            //Search(numbersArray1);
+            //Search(numbersArray2);
+            //Search(numbersArray3);
+        }
+
+        // Reads the files
+        static void ReadFiles()
+        {
+            string Share_1_256 = "C:\\Users\\UpInT\\Source\\Repos\\AlgorithmsandComplexity1\\Algorithms Assignment\\Share_1_256.txt";
+            try
             {
-                dataSet.Add(int.Parse(line));
+                string[] lines = File.ReadAllLines(Share_1_256);
+
+                numbersArray1 = lines.Select(int.Parse).ToArray();
+
+                Console.WriteLine($"{Share_1_256} loaded successfully");
+
+                // Prints the array
+                //Console.WriteLine("Numbers in the array:");
+                //Console.WriteLine(string.Join(", ", numbersArray1));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
             }
 
-            string filePath2 = "C:\\Users\\UpInT\\Source\\Repos\\AlgorithmsandComplexity1\\Algorithms Assignment\\Share_2_256.txt";
-            List<string> lines2 = File.ReadAllLines(filePath).ToList();
-            int[] myNum2 = File.ReadAllLines(filePath).Select(int.Parse).ToArray();
-
-            // Adds data to the data set list
-            foreach (string line in lines2)
+            string Share_2_256 = "C:\\Users\\UpInT\\Source\\Repos\\AlgorithmsandComplexity1\\Algorithms Assignment\\Share_2_256.txt";
+            try
             {
-                dataSet.Add(int.Parse(line));
+                string[] lines = File.ReadAllLines(Share_2_256);
+
+                numbersArray2 = lines.Select(int.Parse).ToArray();
+
+                Console.WriteLine($"{Share_2_256} loaded successfully");
+
+                // Prints the array
+                //Console.WriteLine("Numbers in the array:");
+                //Console.WriteLine(string.Join(", ", numbersArray2));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
             }
 
-            string filePath3 = "C:\\Users\\UpInT\\Source\\Repos\\AlgorithmsandComplexity1\\Algorithms Assignment\\Share_3_256.txt";
-            List<string> lines3 = File.ReadAllLines(filePath).ToList();
-            int[] myNum3 = File.ReadAllLines(filePath).Select(int.Parse).ToArray();
-
-            // Adds data to the data set list
-            foreach (string line in lines3)
+            string Share_3_256 = "C:\\Users\\UpInT\\Source\\Repos\\AlgorithmsandComplexity1\\Algorithms Assignment\\Share_3_256.txt";
+            try
             {
-                dataSet.Add(int.Parse(line));
+                string[] lines = File.ReadAllLines(Share_3_256);
+
+                numbersArray3 = lines.Select(int.Parse).ToArray();
+
+                Console.WriteLine($"{Share_3_256} loaded successfully");
+
+                // Prints the array
+                //Console.WriteLine("Numbers in the array:");
+                //Console.WriteLine(string.Join(", ", numbersArray3));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
             }
 
-            Console.WriteLine(dataSet.Count);
-            Console.WriteLine(string.Join(", ", dataSet));
-            Console.ReadLine();
-
-
-
-            while (true)
+            string Share_1_2048 = "C:\\Users\\UpInT\\Source\\Repos\\AlgorithmsandComplexity1\\Algorithms Assignment\\Share_1_2048.txt";
+            try
             {
-                Console.WriteLine("Menu:");
-                Console.WriteLine("1. Sort");
-                Console.WriteLine("2. Search");
-                string menuInput = Console.ReadLine();
+                string[] lines = File.ReadAllLines(Share_1_2048);
 
-                if (menuInput == "1")
-                    QuickSort(dataSet.ToArray());
-                else if (menuInput == "2")
-                    Search(dataSet.ToArray());
+                numbersArray4 = lines.Select(int.Parse).ToArray();
 
+                Console.WriteLine($"{Share_1_2048} loaded successfully");
+
+                // Prints the array
+                //Console.WriteLine("Numbers in the array:");
+                //Console.WriteLine(string.Join(", ", numbersArray4));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            string Share_2_2048 = "C:\\Users\\UpInT\\Source\\Repos\\AlgorithmsandComplexity1\\Algorithms Assignment\\Share_2_2048.txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(Share_2_2048);
+
+                numbersArray5 = lines.Select(int.Parse).ToArray();
+
+                Console.WriteLine($"{Share_2_2048} loaded successfully");
+
+                // Prints the array
+                //Console.WriteLine("Numbers in the array:");
+                //Console.WriteLine(string.Join(", ", numbersArray5));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            string Share_3_2048 = "C:\\Users\\UpInT\\Source\\Repos\\AlgorithmsandComplexity1\\Algorithms Assignment\\Share_3_2048.txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(Share_3_2048);
+
+                numbersArray6 = lines.Select(int.Parse).ToArray();
+
+                Console.WriteLine($"{Share_3_2048} loaded successfully");
+
+                // Prints the array
+                //Console.WriteLine("Numbers in the array:");
+                //Console.WriteLine(string.Join(", ", numbersArray6));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
             }
         }
 
-        static void QuickSort(int[] array)
+        // To sort array using insertion sort
+        static void InsertionSort(int[] array)
         {
-            Console.WriteLine("Sorting...");
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int temp = array[i]; // The element to be inserted
+                int j = i - 1;
+
+                // Move elements of arr[0..i-1] that are greater than key
+                // to one position ahead of their current position
+                while (j >= 0 && array[j] > temp)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = temp; // Insert the key in the correct position
+            }
         }
 
-        static void Search(int[] array)
+        // Prints array
+        static void PrintArray(int[] array)
         {
-            Console.WriteLine($"Total numbers loaded: {array.Length}");
+            Console.WriteLine("Sorted Array:");
+            foreach (int value in array)
+                Console.Write(value + " ");
+            Console.WriteLine();
+        }
 
-            // {testing} Prints the array - Console.WriteLine("Array: " + string.Join(", ", array));
+        // Search function
+        static void Search(int[] numbersArray)
+        {
+            Console.WriteLine("Search for a number:");
+            string searchedInput = Console.ReadLine();
+            // Might need some error handling for non numbers
+            int searchedNumber = Convert.ToInt32(searchedInput);
+            // searchedNumber = int.MaxValue;
 
-            // Gets search input from user
-            Console.WriteLine("Enter a number to search for:");
-            string searchInput = Console.ReadLine();
-            int searchNumber = Convert.ToInt32(searchInput);
-
-            int index = Array.IndexOf(array, searchNumber);
-
-            // Creates empty list
+            // Creates a new list to store the index positions of the searched number
             List<int> validList = new List<int>();
 
-            // Loops through the array and adds the index of the search number to the list
-            for (int i = 0; i < array.Length; i++)
+            // Loops through the numbers array, incrementing i every time
+            for (int i = 0; i < numbersArray.Length; i++)
             {
-                if (searchNumber == array[i])
+                // If the number is in the array then add the index position to the validList
+                if (searchedNumber == numbersArray[i])
                     validList.Add(i);
             }
-            // Prints array
             if (validList.Count > 0)
                 Console.WriteLine("Index Position: " + string.Join(", ", validList));
             else
-                Console.WriteLine($"'{searchNumber}' was not found");
+                Console.WriteLine($"'{searchedNumber}' was not found");
         }
     }
 }
